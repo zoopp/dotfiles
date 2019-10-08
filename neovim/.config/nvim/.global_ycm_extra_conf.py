af15d367bcae10ee393487e5673b7393cb8bd147
Mihai Bişog <mihai.bisog@gmail.com>
2019-10-08 22:37:08 +0300

    💄 Minor change in formatting

--- neovim/.config/nvim/.global_ycm_extra_conf.py
+++ neovim/.config/nvim/.global_ycm_extra_conf.py
@@ -1,31 +1,29 @@
-# YCM extra conf intended to be used globally. FlagsForFile will attempt to figure out flags in two
-# different ways in the following order:
+# YCM extra conf intended to be used globally. FlagsForFile will attempt to
+# figure out flags in two different ways in the following order:
 #
 #     * from a clang compilation database found in ['compile_commands.json',
-#     'build/compile_commands.json', '_build/compile_commands.json'] somewhere along the parent path
+#     'build/compile_commands.json', '_build/compile_commands.json'] somewhere
+#     along the parent path of the file being edited.
 #
-#     of the file being edited.
-#     * from a .clang_complete file found somewhere along the path of the file being edited.
+#     * from a .clang_complete file found somewhere along the path of the file
+#     being edited.
 #
-# If both method fail then we assume we start with an empty list of flags. In the end, regardless of
-# success or failure of the above methods, we'll append our base flags.
-#
-# Note that while compilation flags for a specific file are cached inside ycmd, this script will
-# attempt to read the compilation database and .clang_complete file every time a new file is opened.
-# This is a trade-off between speed and usability. If this is too slow for you then you should write
-# a project specific .ycm_extra_conf.py for your project or, if you have the time submit a pull
-# request that implements some form of caching that avoids rereads. :)
+# If both method fail then we assume we start with an empty list of flags. In
+# the end, regardless of success or failure of the above methods, we'll append
+# our base flags.
 import os
 import re
 import subprocess
+
 import ycm_core
 
 
-# We won't get suggestions for standard C++ includes unless we explicitly add them to our list of
-# flags.
+# We won't get suggestions for standard C++ includes unless we explicitly add
+# them to our list of flags.
 RE_CLANG_CAPTURE_INCLUDES = \
-    re.compile(r'.*?^#include <\.\.\.> search starts here:\s(?P<paths>.*\s)End of search list.$',
-               re.MULTILINE | re.DOTALL)
+    re.compile(r'.*?^#include <\.\.\.> search starts here:\s'
+               r'(?P<paths>.*\s)'
+               r'End of search list.$', re.MULTILINE | re.DOTALL)
 
 
 def GetStandardIncludes():
@@ -37,7 +35,8 @@ def GetStandardIncludes():
         match = RE_CLANG_CAPTURE_INCLUDES.match(call_result.stderr.decode())
         if match:
             raw_paths = match.group('paths').split()
-            return [''.join(['-isystem', os.path.realpath(path)]) for path in raw_paths]
+            return [''.join(['-isystem', os.path.realpath(path)])
+                    for path in raw_paths]
     return []
 
 
@@ -79,18 +78,21 @@ def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
             if flag.startswith(path_flag):
                 path = flag[len(path_flag):].strip()
                 if not os.path.isabs(path):
-                    new_flags.append(''.join([path_flag, os.path.join(working_directory, path)]))
+                    new_flags.append(''.join([path_flag,
+                                              os.path.join(working_directory,
+                                                           path)]))
                 break
         new_flags.append(flag)
     return new_flags
 
 
 def FindFileUpwards(file_name, start_path, additional_folders=[]):
-    candidates = [file_name] + [os.path.join(folder, file_name) for folder in additional_folders]
-
+    candidates = [file_name] + [os.path.join(folder, file_name)
+                                for folder in additional_folders]
     current_root = os.path.realpath(start_path)
     while current_root != '/':
-        candidate_paths = [os.path.join(current_root, candidate) for candidate in candidates]
+        candidate_paths = [os.path.join(current_root, candidate)
+                           for candidate in candidates]
         for candidate_path in candidate_paths:
             if os.path.exists(candidate_path):
                 return candidate_path
@@ -102,12 +104,14 @@ def GetCompilationInfoForFile(compilation_database, filename):
     if IsHeaderFile(filename):
         basename = os.path.splitext(filename)[0]
         for extension in SOURCE_EXTENSIONS:
-            replacement_files = [basename + extension,
-                                 basename.replace("/include/", "/src/") + extension]
+            replacement_files = [
+                basename + extension,
+                basename.replace("/include/", "/src/") + extension]
             for replacement_file in replacement_files:
                 if os.path.exists(replacement_file):
                     compilation_info = \
-                        compilation_database.GetCompilationInfoForFile(replacement_file)
+                        compilation_database.GetCompilationInfoForFile(
+                            replacement_file)
                     if compilation_info.compiler_flags_:
                         return compilation_info
         return []
@@ -116,7 +120,8 @@ def GetCompilationInfoForFile(compilation_database, filename):
 
 def FlagsFromCompilationDatabase(root, filename):
     try:
-        compilation_db_path = FindFileUpwards('compile_commands.json', root, BUILD_DIRECTORIES)
+        compilation_db_path = FindFileUpwards('compile_commands.json', root,
+                                              BUILD_DIRECTORIES)
         compilation_db_dir = os.path.dirname(compilation_db_path)
         compilation_db = ycm_core.CompilationDatabase(compilation_db_dir)
 
@@ -138,7 +143,8 @@ def FlagsFromClangCompleteFile(root):
         with open(clang_complete_file, 'r') as fd:
             flags = fd.read().splitlines()
 
-        return MakeRelativePathsInFlagsAbsolute(flags, os.path.dirname(clang_complete_file))
+        return MakeRelativePathsInFlagsAbsolute(
+            flags, os.path.dirname(clang_complete_file))
     except:
         return []
 
--- neovim/.config/nvim/autoload/utils.vim
+++ neovim/.config/nvim/autoload/utils.vim
@@ -1,19 +1,14 @@
-
-"
 " Markers to look for in FindProjectRoot
-"
 let s:root_file_markers = [ '.project_root' ]
 let s:root_dir_markers  = [ '.git' ]
 
 
-"
 " Return a path representing the project root or cwd if detection
 " fails.
 "
 " To find the project root we traverse the file system up to the
 " root and we look for certain files at each level. If one such file is
 " found then that path is the project root.
-"
 function! utils#FindProjectRoot()
     for marker in s:root_file_markers
         let path = findfile(marker, '../;')
@@ -33,12 +28,10 @@ function! utils#FindProjectRoot()
 endfunction
 
 
-"
 " Return the current buffer name like so:
 "   if file extension is ext1 then return <buffer_name>.ext2
 "   if file extension is ext2 tehn return <buffer_name>.ext1
 "   else return the buffer name
-"
 function! utils#SwitchBufferExt(ext1, ext2)
     let bufferName = expand('%:t')
     let result = substitute(bufferName, '\.' . a:ext1 . '$', '\.' . a:ext2, 'g')
@@ -48,12 +41,9 @@ function! utils#SwitchBufferExt(ext1, ext2)
     return result
 endfunction
 
-
-"
 " Switch between header and implementation files.
-"   (ext1, ext2) ca be either (header_extension, impl_extension)
+"   (ext1, ext2) can be either (header_extension, impl_extension)
 "   or vice versa.
-"
 function! utils#SwitchHdrImpl(ext1, ext2)
     execute 'FZF -q ' utils#SwitchBufferExt(a:ext1, a:ext2) ' -e -1 -0 ' utils#FindProjectRoot()
 endfunction
